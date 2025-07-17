63fbdc59b64d6b31046c10712f9f7e621870251b
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-17 08:15:46 -0400

    asdf

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -9,15 +9,4 @@ def main(request):
     return HttpResponse(template.render(context, request))
 
 def search(request):
-    if request.method == 'POST':
-        form = MyForm(request.POST)
-        if form.is_valid():
-            textarea_value = form.cleaned_data['my_text_area']
-            # Process the textarea_value (e.g., save to database)
-            return render(request, 'weather_app/blank.html', {'value': textarea_value})
-    else:
-        form = MyForm()
-        
-    return render(request, 'weather_app/main.html', {'form': form})
-    template = loader.get_template('index.html')
-# Create your views here.
+    if request.method == "POST":
