13e3533660097902764291eecd81346996a25c68
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-17 08:03:40 -0400

    asdf

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.main, name="main")
+    path("", views.main, name="main"),
+    path("/<city_name>", views.search, name="city"),
 ]
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .forms import MyForm
 
 def main(request):
     template = loader.get_template('main.html')
     context = {}
     return HttpResponse(template.render(context, request))
-    
+
+def search(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            textarea_value = form.cleaned_data['my_text_area']
+            # Process the textarea_value (e.g., save to database)
+            return render(request, 'weather_app/blank.html', {'value': textarea_value})
+    else:
+        form = MyForm()
+        
+    return render(request, 'weather_app/main.html', {'form': form})
 # Create your views here.
