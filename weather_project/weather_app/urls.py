a0d25255b2416dee0e948e8511f33f86b2f95e57
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-21 12:40:23 -0400

    random random random

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("input/", views.input_view, name="input"),
     path("search/<city_name>/<units>", views.city_view, name="city"),
+    path("input/random", views.random_view, name="random")
 ]
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -5,6 +5,7 @@ from .search import convert_to_imperial
 from .search import search_for_city
 from .search import convert_date
 from datetime import date
+import random
 
 def main(request):
     template = loader.get_template('main.html')
@@ -44,6 +45,54 @@ def input_view(request):
             units = "metric"
         if search_for_city(city_target)['valid'] == True:
             return redirect('city', city_name = city_target, units=units)
+        else:
+            return redirect('city', city_name = "invalid", units = "invalid")
+    
+    template = loader.get_template("blank.html")    
+    return HttpResponse(template.render({}, request))
+
+def random_view(request):
+    if request.method == "POST":
+        
+        cities = [
+    "Tokyo", "Brazil", "Cairo", "Germany", "Nairobi", "Mexico City", "Thailand", "Helsinki", "Lima", "Canada",
+    "Istanbul", "Vietnam", "Jakarta", "Morocco", "Manila", "France", "Buenos Aires", "Norway", "Seoul", "Ethiopia",
+    "Kuala Lumpur", "Italy", "Baghdad", "Russia", "Santiago", "Kenya", "Amsterdam", "India", "Riyadh", "Portugal",
+    "Beijing", "Spain", "Caracas", "Peru", "Athens", "Philippines", "New York", "South Africa", "Tehran", "Denmark",
+    "Stockholm", "Pakistan", "Bangkok", "Colombia", "Oslo", "Algeria", "Addis Ababa", "Netherlands", "Doha", "Ukraine",
+    "Los Angeles", "Saudi Arabia", "Warsaw", "Hungary", "Hanoi", "China", "Kigali", "Sweden", "Abu Dhabi", "Malaysia",
+    "Lagos", "Poland", "Zurich", "Turkey", "Montreal", "Indonesia", "Tunis", "Serbia", "Toronto", "Iran",
+    "Casablanca", "Croatia", "Dubai", "Finland", "Berlin", "Romania", "Vienna", "Sudan", "Barcelona", "Ireland",
+    "Luanda", "Switzerland", "Munich", "Belgium", "Kinshasa", "Czech Republic", "Prague", "Slovakia", "Ankara", "Moroni",
+    "Sofia", "New Delhi", "Zagreb", "Greece", "Cape Town", "Mozambique", "Lusaka", "Slovenia", "Reykjavik", "Cameroon",
+    "Doha", "Estonia", "Panama City", "Ecuador", "Brussels", "Nicaragua", "Kampala", "Guatemala", "San Salvador", "Iceland",
+    "Kabul", "Kuwait City", "Kuwait", "La Paz", "Bolivia", "Caracas", "Samoa", "Bucharest", "Belgrade", "Latvia",
+    "Yerevan", "Georgia", "Quito", "Libya", "Vilnius", "Lithuania", "Skopje", "North Macedonia", "Tbilisi", "Malta",
+    "Ashgabat", "Turkmenistan", "Bratislava", "Kosovo", "Podgorica", "Montenegro", "Monaco", "Andorra", "San Marino", "Liechtenstein",
+    "Suva", "Fiji", "Valletta", "Nicosia", "Cyprus", "Muscat", "Oman", "Manama", "Bahrain", "Kathmandu",
+    "Nepal", "Bhutan", "Thimphu", "Ulaanbaatar", "Mongolia", "Honiara", "Solomon Islands", "Port Vila", "Vanuatu", "Majuro",
+    "Marshall Islands", "Palikir", "Micronesia", "Funafuti", "Tuvalu", "Nuku'alofa", "Tonga", "Port Moresby", "Papua New Guinea", "Apia",
+    "Bangui", "Central African Republic", "Bamako", "Mali", "Ouagadougou", "Burkina Faso", "Freetown", "Sierra Leone", "Monrovia", "Liberia",
+    "Gaborone", "Botswana", "Maseru", "Lesotho", "Bissau", "Guinea-Bissau", "Lilongwe", "Malawi", "Juba", "South Sudan",
+    "Asmara", "Eritrea", "Djibouti", "Djibouti", "Mogadishu", "Somalia", "N'Djamena", "Chad", "Niamey", "Niger",
+    "Yamoussoukro", "Ivory Coast", "Accra", "Ghana", "Lomé", "Togo", "Cotonou", "Benin", "Libreville", "Gabon",
+    "Malabo", "Equatorial Guinea", "Brazzaville", "Congo", "Kigali", "Rwanda", "Bujumbura", "Burundi", "Maputo", "Mozambique",
+    "Harare", "Zimbabwe", "Windhoek", "Namibia", "Pretoria", "South Africa", "Gaza", "Palestine", "Jerusalem", "Israel",
+    "Amman", "Jordan", "Beirut", "Lebanon", "Damascus", "Syria", "Tashkent", "Uzbekistan", "Bishkek", "Kyrgyzstan",
+    "Dushanbe", "Tajikistan", "Nur-Sultan", "Kazakhstan", "Pyongyang", "North Korea", "Seoul", "South Korea", "Taipei", "Taiwan",
+    "Tokyo", "Japan", "Canberra", "Australia", "Wellington", "New Zealand", "Sucre", "Bolivia", "Asunción", "Paraguay",
+    "Montevideo", "Uruguay", "Paramaribo", "Suriname", "Georgetown", "Guyana", "Belmopan", "Belize", "Port-au-Prince", "Haiti",
+    "Kingston", "Jamaica", "Havana", "Cuba", "Santo Domingo", "Dominican Republic", "San Juan", "Puerto Rico", "Panama City", "Panama",
+    "San José", "Costa Rica", "Tegucigalpa", "Honduras", "Managua", "Nicaragua", "Port of Spain", "Trinidad and Tobago", "Castries", "Saint Lucia",
+    "Bridgetown", "Barbados", "Roseau", "Dominica", "Basseterre", "Saint Kitts and Nevis", "St. George's", "Grenada", "Kingstown", "Saint Vincent and the Grenadines",
+    "Victoria", "Seychelles", "Port Louis", "Mauritius", "Antananarivo", "Madagascar", "Moroni", "Comoros", "São Tomé", "São Tomé and Príncipe",
+    "Colombo", "Sri Lanka", "Male", "Maldives", "Singapore", "Singapore", "Honiara", "Solomon Islands", "Tarawa", "Kiribati"
+]
+        
+        city_target = cities[random.randint(0, 299)]
+        
+        if search_for_city(city_target)['valid'] == True:
+            return redirect('city', city_name = city_target, units="metric")
         else:
             return redirect('city', city_name = "invalid", units = "invalid")
     
