c8e00e565aba0a8abdeb1d526ebc2b8210e822d0
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-29 12:55:07 -0400

    sdfg

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.main, name="main"), # homepage
     path("input/", views.input_view, name="input"), # view after inputting a target city
-     path("input/random", views.random_view, name="random"), # view after selecting a random city
+    path("input/random", views.random_view, name="random"), # view after selecting a random city
     path("search/<city_name>/<units>", views.city_view, name="city"), # resulting page after either input_view or random_view are called
 ]
